b53f5354d5b5203dc7dbd98c03e58a243ef9a0e2
Chris Alfonso <calf6244@uni.sydney.edu.au>
2020-12-16 18:03:51 +1100

    Make setup.sh more verbose

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Create necessary directories for inputs and outputs
-for scenario in hybrid no-dyntopo sealevel; do
+echo -n "Creating necessary directories...    "
+for scenario in "hybrid" "sealevel" "no-dyntopo"; do
     if [[ ! -d "inputs/${scenario}/data" ]]; then
         mkdir -p "inputs/${scenario}/data"
     fi
@@ -9,13 +10,16 @@ for scenario in hybrid no-dyntopo sealevel; do
         mkdir -p "results/${scenario}"
     fi
 done
+echo "Done"
 
 # Download data from Zenodo
 [[ ! -d "tmp" ]] && mkdir -p "tmp"
 zip_filename="https://zenodo.org/record/4321853/files/data-bundle.zip"
+echo "Downloading files from ${zip_filename}"
 curl "${zip_filename}" | tar -xz --directory "tmp"
 # Copy to inputs directory
 for scenario in "hybrid" "sealevel" "no-dyntopo"; do
     cp -R "tmp/data-bundle/${scenario}/" "inputs/${scenario}/data/"
 done
 rm -r "tmp"
+echo "Setup completed"
